1948937c6f7bfe1add826917588743829d4c4b50
Swann <astarophty@gmail.com>
2026-04-06 00:52:12 +0200

    add logger and arguments

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,34 @@
+from sys import argv
+from pathlib import Path
+
+from .logger import Logger
+
+
+def _get_path() -> Path:
+    if len(argv) < 2:
+        raise ValueError("No path provided")
+    elif len(argv) > 2:
+        raise ValueError("Too many arguments provided")
+
+    try:
+        path = Path(argv[1])
+    except Exception as e:
+        raise ValueError(f"Invalid path provided: {e}")
+
+    if not path.exists():
+        raise ValueError("Provided path does not exist")
+
+    return path
+
+
 def main():
-    
+    try:
+        path = _get_path()
+    except ValueError as e:
+        Logger.error(e)
+        return
+
+    Logger.info(f"Path provided: {path}")
 
 
 if __name__ == "__main__":
---
+++ src/logger/color.py
@@ -0,0 +1,16 @@
+class Color:
+    RESET = "\033[0m"
+
+    BOLD = "\033[1m"
+    DIM = "\033[2m"
+
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+
+    BG_RED = "\033[41m"
+    BG_GREEN = "\033[42m"
