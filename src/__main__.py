baaf87da69dc4dc36c42bd5eafd819f409eece04
AstarophTY <astarophty@gmail.com>
2026-04-06 11:27:20 +0200

    add error recovery for uknow file

--- src/__main__.py
+++ src/__main__.py
@@ -6,38 +6,36 @@ from .parsing import ConfigLoader
 
 
 def _get_path() -> Path:
-    if len(argv) < 2:
-        raise ValueError("No path provided")
-    elif len(argv) > 2:
-        raise ValueError("Too many arguments provided")
+    if len(argv) > 2:
+        Logger.warning("Too many arguments provided. Using the first one.")
 
     try:
         path = Path(argv[1])
     except Exception as e:
-        raise ValueError(f"Invalid path provided: {e}")
-
-    if not path.exists():
-        raise ValueError("Provided path does not exist")
+        Logger.warning(f"Invalid path provided: {Logger.remove_errno(str(e))}")
 
     return path
 
 
-def main():
-    try:
-        path = _get_path()
-    except ValueError as e:
-        Logger.error(e)
-        return
+def main() -> None:
+    if len(argv) > 2:
+        Logger.warning("Too many arguments provided. Using the first one.")
+    elif len(argv) < 2:
+        Logger.warning("No argument provided.")
+        file = ""
+    else:
+        file = argv[1]
 
-    Logger.info(f"Path provided: {path}")
+    if len(file) > 0:
+        Logger.info(f"Path provided: {file}")
 
     try:
-        config = ConfigLoader.load_config(path)
+        config = ConfigLoader.load_config(str(file))
     except Exception as e:
         Logger.error(f"Error loading config: {e}")
         return
 
-    Logger.debug(config)
+    Logger.debug(str(config))
 
 
 if __name__ == "__main__":
