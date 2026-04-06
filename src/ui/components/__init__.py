f265b028c48deba5ac1a0840f2c3bc8fc4f50c86
Swann <astarophty@gmail.com>
2026-04-07 01:42:04 +0200

    add init

--- src/ui/components/__init__.py
+++ src/ui/components/__init__.py
@@ -1,6 +1,7 @@
 from .background import PacmanBackground
 from .button import MenuButton
 from .logo import MenuLogo
+from .vhs_effect import VHSEffect
 
 
-__all__ = ["PacmanBackground", "MenuButton", "MenuLogo"]
+__all__ = ["PacmanBackground", "MenuButton", "MenuLogo", "VHSEffect"]
