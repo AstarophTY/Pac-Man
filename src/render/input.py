1736a4ead1e7746975705acb5d8bf55dd363ec96
AstarophTY <astarophty@gmail.com>
2026-04-21 11:44:07 +0200

    normes

--- src/render/input.py
+++ src/render/input.py
@@ -1,16 +1,17 @@
 from ursina import application, Entity, time, held_keys
 
+
 class InputHandler(Entity):
     def __init__(self, player):
         super().__init__()
         player.speed = 10
         player.gravity = 0
         self.player = player
-    
+
     def input(self, key):
         if key == 'escape':
             application.quit()
-    
+
     def update(self):
         speed = 10
         if held_keys['c']:
